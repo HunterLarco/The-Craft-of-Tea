b7bf3129dc62b5a872c64c5c1dcb375b9cef42ff
hunterlarco <hjlarco@gmail.com>
2014-09-16 18:09:24 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.appengine.ext.webapp import template
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'pages/main.html')
+		path = os.path.join(os.path.dirname(__file__), 'crawler.html')
 		self.response.out.write(template.render(path, template_values))
 
 
